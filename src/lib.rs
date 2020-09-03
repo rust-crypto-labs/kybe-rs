edc04f4adc018e8834a48ac3b24ad5d7083ccd91
Stanislas Plessia <stplessia@gmail.com>
2020-09-03 23:06:09 +0200

    Fmt(Core): function to snake case

--- src/lib.rs
+++ src/lib.rs
@@ -1,43 +1,42 @@
 extern crate sha3;
 
 mod vector;
-use self::vector::{Vector, Dot};
+use self::vector::{Dot, Vector};
 
 pub struct Poly {}
 pub struct ByteArray {}
 
-
 ////////////// PKE /////////////////////////
 
 // Kyber CPAPKE Key Generation => (secret key, public key)
-pub fn Kyber_CPAPKE_KeyGen() -> (ByteArray, ByteArray){
+pub fn kyber_cpapke_key_gen() -> (ByteArray, ByteArray) {
     unimplemented!();
 }
 
 // Encryption : public key, message, random coins => ciphertext
-pub fn Kyber_CPAPKE_Enc(pk: ByteArray, m: ByteArray, r: ByteArray) -> ByteArray{
+pub fn kyber_cpapke_enc(pk: ByteArray, m: ByteArray, r: ByteArray) -> ByteArray {
     unimplemented!();
 }
 
 // Decryption : secret key, ciphertext => message
-pub fn Kyber_CPAPKE_Dec(sk: ByteArray, c: ByteArray) -> ByteArray{
+pub fn kyber_cpapke_dec(sk: ByteArray, c: ByteArray) -> ByteArray {
     unimplemented!();
 }
 
 ////////////// KEM /////////////////////////
 
 // Kyber CCAKEM Key Generation => (secret key, public key)
-pub fn Kyber_CCAKEM_KeyGen() -> (ByteArray, ByteArray){
+pub fn kyber_ccakem_key_gen() -> (ByteArray, ByteArray) {
     unimplemented!();
 }
 
 // Encryption : public key  => ciphertext, Shared Key
-pub fn Kyber_CCAKEM_Enc(pk: ByteArray) -> (ByteArray, ByteArray) {
+pub fn kyber_ccakem_enc(pk: ByteArray) -> (ByteArray, ByteArray) {
     unimplemented!();
 }
 
 // Decryption : secret key, ciphertext => Shared Key
-pub fn Kyber_CCAKEM_Dec(c: ByteArray, sk: ByteArray) -> ByteArray{
+pub fn kyber_ccakem_dec(c: ByteArray, sk: ByteArray) -> ByteArray {
     unimplemented!();
 }
 
@@ -49,59 +48,59 @@ pub fn parse(bs: ByteArray) -> Poly {
 }
 
 // Centered Binomial Distribution
-pub fn CBD(bs: ByteArray) -> Poly {
+pub fn cbd(bs: ByteArray) -> Poly {
     unimplemented!();
 }
 
 // Serialize Polynomial into ByteArray
-pub fn Encode(p: Poly) -> ByteArray {
+pub fn encode(p: Poly) -> ByteArray {
     unimplemented!();
 }
 
 // Deserialize ByteArray into Polynomial
-pub fn Decode(bs: ByteArray) -> Poly {
+pub fn decode(bs: ByteArray) -> Poly {
     unimplemented!();
 }
 
 // Pseudo random function => SHAKE-256(s||b);
-fn PRF(){
+fn prf() {
     unimplemented!();
 }
 
 // Extendable output function => SHAKE-128
-fn XOF(){
+fn xof() {
     unimplemented!();
 }
 
 // Hash function => SHA3-256
-fn H(){
+fn h() {
     unimplemented!();
 }
 
 // Hash function => SHA3-512
-fn G(){
+fn g() {
     unimplemented!();
 }
 
 // Key Derivation function => SHAKE-256
-fn KDF(){
+fn kdf() {
     unimplemented!();
 }
 
 // Number theoretic Transform
-fn NTT(){
+fn ntt() {
     unimplemented!();
 }
 
 // Reverse NTT
-fn RevNTT(){
+fn rev_ntt() {
     unimplemented!();
 }
 
-fn Compress(x: u64, d: u64) -> u64 {
+fn compress(x: u64, d: u64) -> u64 {
     unimplemented!();
 }
 
-fn Decompress(x: u64, d: u64) -> u64 {
+fn decompress(x: u64, d: u64) -> u64 {
     unimplemented!();
 }
