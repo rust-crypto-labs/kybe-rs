bd1f3016c0c6fa9a82443c392bf5e300027c6a4d
r.g.s <anx99@rgs.com>
2020-09-16 08:43:50 +0200

    Cargo fmt

--- src/compress.rs
+++ src/compress.rs
@@ -1,16 +1,16 @@
-use crate::{PrimeField3329, Poly3329, PolyVec3329};
+use crate::{Poly3329, PolyVec3329, PrimeField3329};
 
 /// Compress function on coefficients, p. 6
 pub fn compress_integer(x: i64, d: usize, q: usize) -> i64 {
     let m = 1 << d;
-    let f = (m as f64)/(q as f64);
+    let f = (m as f64) / (q as f64);
     let f = f * (x as f64);
 
     let mut c = (f.round() as i64) % m;
 
     // We now need to compute the mod+ variant; this is not
     // constant time
-    if 2*c >= m {
+    if 2 * c >= m {
         c -= m;
     }
 
@@ -20,8 +20,8 @@ pub fn compress_integer(x: i64, d: usize, q: usize) -> i64 {
 /// Decompress function on coefficients, p. 6
 pub fn decompress_integer(x: i64, d: usize, q: usize) -> i64 {
     let m = 1 << d;
-    let f = (q as f64 * x as f64)/(m as f64);
-    
+    let f = (q as f64 * x as f64) / (m as f64);
+
     f.round() as i64
 }
 
@@ -29,7 +29,11 @@ pub fn decompress_integer(x: i64, d: usize, q: usize) -> i64 {
 pub fn compress_poly(x: Poly3329, d: usize, q: usize) -> Poly3329 {
     let mut coeffs = vec![];
     for xi in x.coefficients.iter() {
-        coeffs.push(PrimeField3329::from_int(compress_integer(xi.to_int(), d, q)));
+        coeffs.push(PrimeField3329::from_int(compress_integer(
+            xi.to_int(),
+            d,
+            q,
+        )));
     }
     Poly3329::from_vec(coeffs, x.dimension())
 }
@@ -38,7 +42,11 @@ pub fn compress_poly(x: Poly3329, d: usize, q: usize) -> Poly3329 {
 pub fn decompress_poly(x: Poly3329, d: usize, q: usize) -> Poly3329 {
     let mut coeffs = vec![];
     for xi in x.coefficients.iter() {
-        coeffs.push(PrimeField3329::from_int(decompress_integer(xi.to_int(), d, q)));
+        coeffs.push(PrimeField3329::from_int(decompress_integer(
+            xi.to_int(),
+            d,
+            q,
+        )));
     }
     Poly3329::from_vec(coeffs, x.dimension())
 }
@@ -51,7 +59,6 @@ pub fn compress_polyvec(x: PolyVec3329, d: usize, q: usize) -> PolyVec3329 {
     }
     PolyVec3329::from_vec(coeffs)
 }
-
 
 /// Decompress function on R_q^k
 fn decompress_polyvec(x: PolyVec3329, d: usize, q: usize) -> PolyVec3329 {
