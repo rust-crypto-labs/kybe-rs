2070b52e999a06f47b6f1a6f2d3ea82ed1e15d67
Ashashin <edmond.de-roffignac@student.ecp.fr>
2020-10-01 23:08:12 +0200

    Update vector  multiplication

--- src/ntt.rs
+++ src/ntt.rs
@@ -6,9 +6,29 @@ pub fn bcm(_a: &Poly3329, _b: &Poly3329) -> Poly3329 {
     unimplemented!()
 }
 
+pub fn bcm_vec(a: &PolyVec3329, b:&PolyVec3329) -> Poly3329 {
+    let l = a.dimension();
+    assert_eq!(l, b.dimension());
+
+    let mut p = bcm(&a.get(0), &b.get(0));
+    for i in 1..l {
+        p = p.add(&bcm(&a.get(i), &b.get(i)));
+    }
+    p
+}
+
 /// Matrix basecase multiplication, cf p. 7
-pub fn bcm_matrix_vec(_a: &PolyMatrix3329, _b: &PolyVec3329) -> PolyVec3329 {
-    unimplemented!()
+pub fn bcm_matrix_vec(a: &PolyMatrix3329, b: &PolyVec3329) -> PolyVec3329 {
+    let (x,y) = a.dimensions();
+    assert_eq!(x, b.dimension());
+
+    let mut v = PolyVec3329::init(y);
+
+    for i in 0..y {
+        v.set(i,bcm_vec(&a.row(i), &b))
+    }
+
+    v
 }
 
 /// Computes a.b as NTT^-1(a_hat o b_hat)
@@ -18,20 +38,12 @@ pub fn ntt_product(a_hat: &Poly3329, b_hat: &Poly3329) -> Poly3329 {
 
 /// Computes a^T.b as NTT^-1(a_hat^T o b_hat)
 pub fn ntt_product_vec(a_hat: &PolyVec3329, b_hat: &PolyVec3329) -> Poly3329 {
-
-    let l = a_hat.dimension();
-    assert_eq!(l, b_hat.dimension());
-
-    let mut p = bcm(&a_hat.get(0), &b_hat.get(0));
-    for i in 1..l {
-        p = p.add(&bcm(&a_hat.get(i), &b_hat.get(i)));
-    }
-    rev_ntt(&p)
+    rev_ntt(&bcm_vec(a_hat,b_hat))
 }
 
 /// Computes a.b as NTT^-1(a_hat o b_hat)
-pub fn ntt_product_matvec(_a_hat: &PolyMatrix3329, _b_hat: &PolyVec3329) -> PolyVec3329 {
-    unimplemented!()
+pub fn ntt_product_matvec(a_hat: &PolyMatrix3329, b_hat: &PolyVec3329) -> PolyVec3329 {
+    rev_ntt_vec(&bcm_matrix_vec(a_hat, b_hat))
 }
 
 // Number theoretic Transform on vectors
