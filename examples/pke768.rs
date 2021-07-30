db6a22d47f805714315690f22d53d64dd26c012f
Ashashin <edmond.de-roffignac@student.ecp.fr>
2021-07-31 01:00:36 +0200

    fmt

--- examples/pke768.rs
+++ examples/pke768.rs
@@ -1,16 +1,10 @@
-use kybe_rs::{
-    ByteArray,
-    KyberParams,
-    kyber_cpapke_key_gen,
-    kyber_cpapke_enc,
-    kyber_cpapke_dec,
-};
+use kybe_rs::{kyber_cpapke_dec, kyber_cpapke_enc, kyber_cpapke_key_gen, ByteArray, KyberParams};
 
 fn main() {
     let params = KyberParams::kyber768();
     let m = ByteArray::random(32);
     let r = ByteArray::random(32);
-    
+
     let (sk, pk) = kyber_cpapke_key_gen(params);
     let enc = kyber_cpapke_enc(params, &pk, &m, r.clone());
     let dec = kyber_cpapke_dec(params, &sk, &enc);
